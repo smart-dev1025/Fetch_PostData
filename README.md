8619c87e8b802ff629f90e1142c69c7b7f997079
smart-dev318 <62882534+smart-dev318@users.noreply.github.com>
2022-06-04 17:20:53 -0600

    how to axios

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Fetch_PostData
-Created with CodeSandbox
+
+Fetch the data using axios and render a list
+https://jsonplaceholder.typicode.com/posts
+
+- The list will be a list of cards
+- Clicking on each card will route to the post's details
+- Implement using TypeScript
+- Implement Redux (optional)
+- Implement basic styling
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,43 @@
 import "./styles.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+interface User {
+  id: number;
+  userId: number;
+  title: string;
+  body: string;
+}
 
 export default function App() {
+  const [user, setUser] = useState<User[] | null>(null);
+  const baseUrl = "https://jsonplaceholder.typicode.com/posts";
+  const fetchData = async () => {
+    let response = await axios.request({
+      url: baseUrl,
+      method: "get"
+    });
+    if (response.status === 200) {
+      setUser(response.data);
+    }
+  };
+
+  useEffect(() => {
+    console.log("!!!!!!!!");
+    fetchData().then();
+  }, []);
+
   return (
     <div className="App">
       <h1>This is list posted</h1>
+      {user &&
+        user.map((item) => {
+          return (
+            <p key={item.id}>
+              <a href={`${baseUrl}/${item.id}`}>{item.title}</a>
+            </p>
+          );
+        })}
     </div>
   );
 }
