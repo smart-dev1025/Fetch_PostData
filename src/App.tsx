467dcede3d4bb29e6f65538073cbb984ea1715f3
smart-dev318 <62882534+smart-dev318@users.noreply.github.com>
2022-06-04 17:22:25 -0600

    code rearrange.

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ interface User {
 }
 
 export default function App() {
-  const [user, setUser] = useState<User[] | null>(null);
+  const [user, setUser] = useState<User[]>([]);
   const baseUrl = "https://jsonplaceholder.typicode.com/posts";
   const fetchData = async () => {
     let response = await axios.request({
@@ -23,21 +23,19 @@ export default function App() {
   };
 
   useEffect(() => {
-    console.log("!!!!!!!!");
     fetchData().then();
   }, []);
 
   return (
     <div className="App">
       <h1>This is list posted</h1>
-      {user &&
-        user.map((item) => {
-          return (
-            <p key={item.id}>
-              <a href={`${baseUrl}/${item.id}`}>{item.title}</a>
-            </p>
-          );
-        })}
+      {user.map((item) => {
+        return (
+          <p key={item.id}>
+            <a href={`${baseUrl}/${item.id}`}>{item.title}</a>
+          </p>
+        );
+      })}
     </div>
   );
 }
